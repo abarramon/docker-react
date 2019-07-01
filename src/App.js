81a3527f1f0af891b562cc7ac1081a6fdcc6989f
Alberto Barranco Ramón <abarranco@iMac-de-Alberto.local>
2019-07-02 00:05:51 +0200

    change to see if CD is working

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CONTINUOUS DEPLOYMEN ACHIEVED !!!!
+          ALBERTO BEsT !!!!
         </p>
         <a
           className="App-link"
