f7b5b391f464d0ea82b242e21a637ea57f2d06e6
Alberto Barranco Ramón <abarranco@iMac-de-Alberto.local>
2019-07-01 22:10:59 +0200

    deleted build folder, added gitignore

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BUBBUBU
+          CONTINUOUS DEPLOYMEN ACHIEVED !!!!
         </p>
         <a
           className="App-link"
